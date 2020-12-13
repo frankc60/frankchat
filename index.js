360ab5033b3c4fd47e84ddcf2429909f17206439
frankc60 <frankc60@gmail.com>
2020-12-13 17:46:36 +1300

    intial - express + ejs

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const ejs = require("ejs");
+
+var app = express();
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+app.listen(8080);
+console.log("8080 is the magic port");
